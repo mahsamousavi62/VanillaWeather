f86e8817fc24ce6f2f6b94600840ac6722bd285d
mahsamousavi62 <47369136+mahsamousavi62@users.noreply.github.com>
2022-12-23 13:18:36 +0330

    init2

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
+let d = new Date();
 
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+function getMinutes() {
+  let minute = d.getMinutes();
+  if (minute < 10) {
+    minute = 0 + minute.toString();
+  }
+  return minute;
+}
+
+document.getElementById("time").innerHTML = `${days[d.getDay()]}
+ ${d.getHours()}:${getMinutes()}`;
