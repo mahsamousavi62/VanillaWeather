eef52fdc78bf44b19d7fab7d2a43812f8dc11fb9
mahsamousavi62 <47369136+mahsamousavi62@users.noreply.github.com>
2022-12-24 14:03:07 +0330

    searchEngine

--- src/app.js
+++ src/app.js
@@ -1,90 +1,59 @@
-let apiKey = "1ee4264117b73d2263eecd562f31ef5c";
+let apiKey = "831145553ta05682f82of176f82b2b14";
 
-// function formatDate(date) {
-//   let hours = date.getHours();
-//   if (hours < 10) {
-//     hours = `0${hours}`;
-//   }
-//   let minutes = date.getMinutes();
-//   if (minutes < 10) {
-//     minutes = `0${minutes}`;
-//   }
+let celciosTempreture = 0;
 
-//   let dayIndex = date.getDay();
-//   let days = [
-//     "Sunday",
-//     "Monday",
-//     "Tuesday",
-//     "Wednesday",
-//     "Thursday",
-//     "Friday",
-//     "Saturday",
-//   ];
-//   let day = days[dayIndex];
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-//   return `${day} ${hours}:${minutes}`;
-// }
-//document.getElementById("time").innerHTML = formatDate(new Date());
-function fetchForecast() {
-  var endpoint = `https://api.openweathermap.org/data/2.5/onecall?lat=38.7267&lon=-9.1403&exclude=current,hourly,minutely,alerts&units=metric&appid=${apiKey}`;
-  var forecastEl = document.getElementsByClassName("forecast");
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
 
-  axios
-    .get(endpoint)
-    .then(function (response) {
-      if (200 !== response.status) {
-        console.log(
-          "Looks like there was a problem. Status Code: " + response.status
-        );
-        return;
-      }
-
-      forecastEl[0].classList.add("loaded");
-
-      var fday = "";
-      response.data.daily.forEach((value, index) => {
-        if (index > 0) {
-          var dayname = new Date(value.dt * 1000).toLocaleDateString("en", {
-            weekday: "long",
-          });
-          var icon = value.weather[0].icon;
-          var temp = value.temp.day.toFixed(0);
-          fday = `<div class="forecast-day">
-						<p>${dayname}</p>
-						<p><span class="ico-${icon}" title="${icon}"></span></p>
-						<div class="forecast-day--temp">${temp}<sup>°C</sup></div>
-					</div>`;
-          forecastEl[0].insertAdjacentHTML("beforeend", fday);
-        }
-      });
-    })
-    .catch(function (err) {
-      console.log("Fetch Error :-S", err);
-    });
+  return `${day} ${hours}:${minutes}`;
 }
-let form = document.querySelector("form");
-form.addEventListener("submit", function (event) {
+
+function call(event) {
   event.preventDefault();
-  fetchForecast();
-  //   https: axios
-  //     .get(
-  //       `https://api.openweathermap.org/data/2.5/weather?q=${
-  //         document.getElementById("searchcity").value
-  //       }&appid=${apiKey}&units=metric&exclue=currnt,daily`
-  //     )
-  //     .then(function (response) {
-  //       debugger;
-  //       response.data.dt * 1000;
-  //       let temp = Math.round(response.data.main.temp);
-  //       let description = response.data.weather[0].description;
-  //       document.querySelector("#city").innerHTML = response.data.name;
-  //       document.querySelector("#description").innerHTML = `${description}`;
-  //       document.querySelector("#temperature").innerHTML = `${temp}`;
-  //       document.querySelector(
-  //         "#humidity"
-  //       ).innerHTML = `${response.data.main.humidity}%`;
-  //       document.querySelector("#wind").innerHTML = `${Math.round(
-  //         response.data.wind.speed
-  //       )}km/h`;
-  //     });
-});
+
+  city = document.getElementById("city-input").value;
+  let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(url).then(ShowTempreture);
+}
+
+function ShowTempreture(response) {
+  //   console.log(response.data);
+
+  document
+    .getElementById("icon")
+    .setAttribute("src", response.data.condition.icon_url);
+  let time = new Date(response.data.time * 1000);
+  document.getElementById("date").innerHTML = formatDate(time);
+  document.getElementById("city").innerHTML = response.data.city;
+
+  celciosTempreture = Math.round(response.data.temperature.current);
+  document.getElementById("temperature").innerHTML = celciosTempreture;
+  document.getElementById("humidity").innerHTML =
+    response.data.temperature.humidity;
+  document.getElementById("wind").innerHTML = response.data.wind.speed;
+  document.getElementById("description").innerHTML =
+    response.data.condition.description;
+}
+
+let form = document.getElementById("search-form");
+form.addEventListener("submit", call);
