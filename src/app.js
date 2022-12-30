4169227a1ecf91b2a44b05c6dc25c1aeb3e9eaee
mahsamousavi62 <47369136+mahsamousavi62@users.noreply.github.com>
2022-12-30 14:18:04 +0330

    remove F tempretur

--- src/app.js
+++ src/app.js
@@ -89,11 +89,7 @@ function ShowTempreture(response) {
 
   displayForcast(response.data);
 }
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
 let form = document.getElementById("search-form");
 form.addEventListener("submit", call);
 
